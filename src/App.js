553e997ada724160556b075286dcc2c898d08f57
LGeoff31 <geoffrey31415@gmail.com>
2023-11-16 01:59:11 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header/Header";
 
-function App() {
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+import { Grid } from "@material-ui/core";
+import CssBaseLine from "@mui/material/CssBaseline";
+import List from "./components/List/List";
+import { getPlacesData } from "./api";
+
+const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const filteredPlaces = places.filter((place) => place.rating > rating);
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+  useEffect(() => {
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
+      // console.log(coordinates, bounds.ne.lat, bounds.sw.lat);
+      // console.log(coordinates, bounds.sw, bounds.ne);
+      getPlacesData(type, bounds?.sw, bounds?.ne).then((data) => {
+        console.log("data", data);
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setIsLoading(false);
+      });
+    }
+  }, [bounds, type]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CssBaseLine />
+      <Header setCoordinates={setCoordinates} />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={filteredPlaces.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
+          />
+        </Grid>
+      </Grid>
+
+      {/* <PlaceDetails /> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,60 @@
+import axios from "axios"; //help make api cals
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+const options = {
+  params: {
+    bl_latitude: "11.847676",
+    tr_latitude: "12.838442",
+    bl_longitude: "109.095887",
+    tr_longitude: "109.149359",
+  },
+  headers: {
+    "X-RapidAPI-Key": "008c66178emsh836ccbe91419dd8p18cf0ejsnd202cfed8f01",
+    "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+  },
+};
+
+// const URL =
+//   "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+// const options = {
+//   url: URL,
+//   params: {
+//     bl_latitude: "11.847676",
+//     tr_latitude: "12.838442",
+//     bl_longitude: "109.095887",
+//     tr_longitude: "109.149359",
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": "aa585c949bmshb89eb22f6535c22p176a9ajsndc4fd6eba0a1",
+//     "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+//   },
+// };
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    //request
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        url: URL,
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "X-RapidAPI-Key":
+            "008c66178emsh836ccbe91419dd8p18cf0ejsnd202cfed8f01",
+          "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
